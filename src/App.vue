874e389ab5d434f14d4e5bad81fdfe186d282f34
Dan97O <dan97-oddo@live.it>
2023-06-13 16:15:57 +0200

    add bonus paginate

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
     data() {
         return {
             loading: true,
-            projects: null,
+            projects: [],
             error: null,
             base_url: 'http://127.0.0.1:8000/',
             projects_API: 'api/projects',
@@ -20,15 +20,24 @@ export default {
             axios
                 .get(url)
                 .then(response => {
-                    console.log(response);
+                    //console.log(response);
                     this.projects = response.data.projects
                     this.loading = false
                 })
                 .catch(error => {
-                    console.log(error);
+                    //console.log(error);
                     this.error = error.message
                 })
         },
+
+        prevPage(path) {
+            console.log(path);
+            this.getProject(path)
+        },
+        nextPage(path) {
+            this.getProject(path)
+        },
+
     },
     mounted() {
         const url = this.base_url + this.projects_API
@@ -42,13 +51,36 @@ export default {
     <section class="projects">
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
-                <div class="col" v-for="project in  projects ">
+                <div class="col" v-for="project in  projects.data ">
 
                     <ProjectCard :project="project"></ProjectCard>
 
+
                 </div>
+
             </div>
         </div>
+
+        <div class="container">
+            <nav aria-label="Page navigation">
+                <ul class="pagination    ">
+                    <li class="page-item">
+                        <button class="page-link" aria-label="Previous" v-if="projects.prev_page_url"
+                            @click="prevPage(projects.prev_page_url)">
+                            <span aria-hidden="true">&laquo;</span>
+                        </button>
+                    </li>
+                    <li class="page-item">
+                        <button class="page-link" aria-label="Next" v-if="projects.next_page_url"
+                            @click="nextPage(projects.next_page_url)">
+                            <span aria-hidden="true">&raquo;</span>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+
     </section>
 </template>
 

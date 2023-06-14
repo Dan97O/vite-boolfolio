8f54801b0e4549760814bc1fa51884c3631df0b9
Dan97O <dan97-oddo@live.it>
2023-06-14 14:35:55 +0200

    add component AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script>
-
-
+import AppHeader from './components/AppHeader.vue';
 export default {
     components: {
-
-    },
-
+        AppHeader
+    }
 }
 </script>
 
 <template>
-    <header>
-        <nav class="nav justify-content-center">
-            <router-link class="nav-link" :to="{ 'name': 'home' }" aria-current="page">Home</router-link>
-            <router-link class="nav-link" :to="{ 'name': 'about' }" aria-current="page">About</router-link>
-            <router-link class="nav-link" :to="{ 'name': 'contacts' }" aria-current="page">Contact</router-link>
-        </nav>
-    </header>
+    <AppHeader></AppHeader>
     <router-view></router-view>
-    <!--  <HomeView></HomeView> -->
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+<template>
+  <header>
+    <nav class="nav justify-content-center">
+      <router-link class="nav-link" :to="{ 'name': 'home' }" aria-current="page">Home</router-link>
+      <router-link class="nav-link" :to="{ 'name': 'about' }" aria-current="page">About</router-link>
+      <router-link class="nav-link" :to="{ 'name': 'contacts' }" aria-current="page">Contact</router-link>
+    </nav>
+  </header>
+</template>
+
+
+<style lang="scss" scoped></style>
